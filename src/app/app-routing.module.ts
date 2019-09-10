eeb52cf8b75c8e880e66500c9d9b882b4a9317e2
lekhachanh <lekhachanh123@gmail.com>
2019-09-10 10:33:49 +0700

    add commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {UserloginComponent} from './userlogin/userlogin.component';
+import {RegisterComponent} from './register/register.component';
+import {CustomerManagementComponent} from './customer-management/customer-management.component';
+import {CreateCustomerComponent} from './create-customer/create-customer.component';
+
+
+
+
+const routes: Routes = [
+  {path: 'login', component: UserloginComponent},
+  {path: 'customer-management', component: CustomerManagementComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'create-customer', component: CreateCustomerComponent}
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
